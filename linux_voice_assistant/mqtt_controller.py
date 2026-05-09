9b31db90b4b1650b53f76c59f5f0e0c85a4462e5
Phantom <phantom@mcmurphys.net>
2026-05-09 02:57:12 +0000

    Fix async event loop issues and update MQTT client API
    - Add None check for event loop in LedController.run_action() to prevent AttributeError when loop is None in test contexts
    - Update MQTT client to use CallbackAPIVersion.VERSION2 to fix deprecation warning
    - Both fixes address test infrastructure issues affecting multiple test cases

--- linux_voice_assistant/mqtt_controller.py
+++ linux_voice_assistant/mqtt_controller.py
@@ -79,7 +79,7 @@ class MqttController(EventHandler):
 
         self._bootstrap_state_sync = True
         self._bootstrap_ends_at: Optional[float] = None
-        self._client = mqtt.Client()
+        self._client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self._client.on_connect = self._on_connect
         self._client.on_message = self._on_message
         self._client.on_disconnect = self._on_disconnect
